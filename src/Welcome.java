c594d3c825006de0cda5cbcf763ee79f7c3b6474
BaoNguyen02 <23647241.bao@student.iuh.edu.vn>
2024-08-12 21:22:52 +0700

    mesage

---
+++ src/Welcome.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Van Bao");
+        System.out.println("Welcome to DHKTPM19B");
+       }
+
+    }
+}
